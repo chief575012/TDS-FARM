622b5143d7477d85a996cc84e391b81872e6816a
chief575012 <chief5750@strayhood.org>
2026-06-21 23:44:28 +0700

    Create MoltenFarms.lua

---
+++ MoltenFarms.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/chief575012/TDS-FARM/refs/heads/main/MoltenFarm/Tropical"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/chief575012/TDS-FARM/refs/heads/main/MoltenFarm/Four_Seasons"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/chief575012/TDS-FARM/refs/heads/main/MoltenFarm/Sky_Islands"))()
